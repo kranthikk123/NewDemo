bbb51ade9caef8a410c8ecdf4e9753dbce62aa33
KranthiKumar <kranthikumarcse233@gmail.com>
2023-08-11 22:05:02 +0530

    THis is user1 commit

--- src/main/java/a/Mapss.java
+++ src/main/java/a/Mapss.java
@@ -5,7 +5,7 @@ public class Mapss {
 	public static void main(String[] args) {
 		System.out.println("This is KranthiKumar");
 		System.out.println("Jesus");
-
+		System.out.println("I love you Jesus");
 	}
 
 }

fa4d0b5855142e5eb96a810a05c1fa8086d317e1
KranthiKumar <kranthikumarcse233@gmail.com>
2023-08-13 20:26:44 +0530

    this is on 1308

--- src/test/java/Mapss2.java
+++ src/test/java/Mapss2.java
@@ -7,6 +7,7 @@ public class Mapss2 {
 		System.out.println("This is KranthiKumar");
 		System.out.println("Jesus");
 		System.out.println("I love you Jesus");
+		System.out.println("This test is from SMOKE");
 	}
 
 }
